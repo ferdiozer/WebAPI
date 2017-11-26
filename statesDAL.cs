c52f2b0b5f28ffc85910948bc9841fd7a3ac235c
Ferdi Özer <mysteriously04@gmail.com>
2017-11-27 02:05:06 +0300

    CRUD işlemleri
    DAL  içerisinde mobil_state (bu benim telefon durumu senaryo) Entity Framework Kullanılarak oluşturulan crud fonksiyonları

---
+++ statesDAL.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfControl.DAL
+{//Telefon Durum(mobil_state) veritabanı işlemleri    //       CRUD
+    public class statesDAL
+    {
+        //EF ile veritabanı bağlantısı
+        SelfControlDBEntities db = new SelfControlDBEntities();
+        
+        //Tüm durumları Getir
+        public IEnumerable<mobil_state> getAll()
+        {
+            return db.mobil_state;
+        }
+
+        // İd ye göre durum getir
+        public mobil_state getById(int id)
+        {
+            return db.mobil_state.Find(id);
+        }
+
+        // Ekleme işlemi (eklenen nesneyi Geri Döndür)
+        public mobil_state create(mobil_state s)
+        {
+            db.mobil_state.Add(s);
+            db.SaveChanges();
+
+            return s;
+        }
+
+        // Güncelleme
+        public mobil_state update(int id,mobil_state s)
+        {
+            db.Entry(s).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+
+            return s;
+        }
+
+        //Silme
+        public void delete(int id)
+        {       //gelen id ye ait bir surum bulması durumunda silecektir
+            db.mobil_state.Remove(db.mobil_state.Find(id));
+            db.SaveChanges();
+        }
+    }
+}
